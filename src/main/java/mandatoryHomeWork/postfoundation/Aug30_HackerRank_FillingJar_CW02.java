e655ff951b13b0cd9f4f9be11c70cf114e90f8b6
RajalakshmiGR <rajalakshmisdet@gmail.com>
2023-08-31 21:57:33 +0530

    Aug 30CWdone

--- src/main/java/mandatoryHomeWork/postfoundation/Aug30_HackerRank_FillingJar_CW02.java
+++ src/main/java/mandatoryHomeWork/postfoundation/Aug30_HackerRank_FillingJar_CW02.java
@@ -1,35 +1,87 @@
-package mandatoryHomeWork.postfoundation;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-import java.util.ArrayList;
+class Result {
 
-public class Aug30_HackerRank_FillingJar_CW02 {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ArrayList<ArrayList<Integer>> array_list = new ArrayList<ArrayList<Integer>>();
-		for (int i = 0; i < 3; i++) {
-			ArrayList<Integer> sub_array_list = new ArrayList<Integer>();
-			// The space for the 0th roArrayListw can be allocated with the use of new keyword, this is done in this line. The 0th row also allows the store of 0 value as default .
-			//array_list.add(new ArrayList<Integer>());
-			//array_list.add(new ArrayList<Integer>());
-			sub_array_list.add(0, 1);
-			sub_array_list.add(1, 2);
-			sub_array_list.add(2, 100);
-			
-			array_list.add(sub_array_list);
-		}
-		System.out.println(array_list.size());
-		
-		System.out.println(array_list);
-		
-		  for(int i=0;i<array_list.size();i++){
-               System.out.println(array_list.get(i));
-           } 
-                
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY operations
+     */
 
-	}
+    public static int solve(int n, List<List<Integer>> operations) {
+    // Write your code here
+        
+        int start_index=0;
+        int end_index=0;
+        int filler =0;
+        int sum=0;
+        
+        for(List<Integer> temp: operations)
+        {
+            start_index=temp.get(0);
+            end_index=temp.get(1);
+            filler=temp.get(2);
+            
+            System.out.println(start_index);
+            System.out.println(end_index);
+            System.out.println(filler);
+            for(int i=start_index;i<=end_index;i++)
+            {
+                sum+=filler;
+            }
+            
+        }
+        System.out.println(sum);
+     
+      return sum/n;
+    }
 
 }
 
-/**/
+public class Aug30_HackerRank_FillingJar_CW02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> operations = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                operations.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.solve(n, operations);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
