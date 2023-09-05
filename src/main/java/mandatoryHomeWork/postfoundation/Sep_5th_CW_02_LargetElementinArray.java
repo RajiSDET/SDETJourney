7c7e7e08cbb8b0fe87cab2f4d5080167f7727ebf
rramiah-ext <rramiah-ext@drivewealth.tech>
2023-09-05 07:41:21 +0530

    SEP5ActivityCW2

---
+++ src/main/java/mandatoryHomeWork/postfoundation/Sep_5th_CW_02_LargetElementinArray.java
@@ -0,0 +1,79 @@
+package mandatoryHomeWork.postfoundation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Sep_5th_CW_02_LargetElementinArray {
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals(5,LargestEle(new int[] {1,5,3,2,4}) );
+	}
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals(8,LargestEle(new int[] {4,7,8,6,7,6}) );
+	}
+	
+	public int LargestEle(int[] inputarr)
+	{
+		int max=0;
+		
+		for (int i = 0; i < inputarr.length; i++) {
+			if(max<inputarr[i])
+			{
+				max=inputarr[i];
+			}
+		}
+		return max;
+	}
+
+}
+
+/*
+ Pseudo Code 
+ 
+ scan through the array 
+ let max = 0 to start with 
+ now compare max with each element in the array 
+ replace the max with inputarray[i] whenever inputarray[i] is greater than max 
+ 
+ */
+
+/*https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279
+ * 
+ Largest Element in the Array
+Last Updated: 23 Aug, 2023
+Easy
+0/40
+10 mins
+90 %
+114 upvotes
++6 more
+Problem Statement
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+6
+4 7 8 6 7 6 
+Sample Output 1:
+8
+Explanation Of Sample Input 1:
+The answer is 8.
+From {4 7 8 6 7 6}, 8 is the largest element.
+Sample Input 2:
+8
+5 9 3 4 8 4 3 10 
+Sample Output 2:
+10
+Expected Time Complexity:
+O( N ), Where ‘N’ is the size of an input array ‘arr’.
+Constraints :
+1 <= 'n' <= 10^5
+1 <= 'arr[i]' <= 10^9
+
+Time Limit: 1 sec
+ * 
+ * */
+ 
