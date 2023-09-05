b752d0f874f9e116b143dd9a46a4c02683a0d27e
rramiah-ext <rramiah-ext@drivewealth.tech>
2023-09-05 08:12:11 +0530

    SEP5ActivityHW1

--- src/main/java/mandatoryHomeWork/postfoundation/Sep5th_HW_01_String_is_Acronym.java
+++ src/main/java/mandatoryHomeWork/postfoundation/Sep5th_HW_01_String_is_Acronym.java
@@ -1,11 +1,52 @@
 package mandatoryHomeWork.postfoundation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class Sep5th_HW_01_String_is_Acronym {
 	
+	@Test
+	public void test1()
+	{
+		ArrayList<String> words = new ArrayList<String>(Arrays. asList("alice","bob","charlie"));
+		Assert.assertEquals(true,isAcronym(words,"abc") );
+	}
+	
+	@Test
+	public void test2()
+	{
+		ArrayList<String> words = new ArrayList<String>(Arrays. asList("an","apple"));
+		Assert.assertEquals(false,isAcronym(words,"a") );
+	}
+	
+	@Test
+	public void test3()
+	{
+		ArrayList<String> words = new ArrayList<String>(Arrays. asList("never","gonna","give","up","on","you"));
+		Assert.assertEquals(true,isAcronym(words,"ngguoy") );
+	}
+	
 public boolean isAcronym(List<String> words, String s) {
-        return false;
+	StringBuilder sb = new StringBuilder();
+	
+	for (int i = 0; i < words.size(); i++) {
+		String temp = words.get(i);
+		sb.append(temp.charAt(0));
+		
+	}
+	System.out.println(sb);
+	
+	if(sb.toString().equals(s))
+	{
+		return true;
+	}
+        
+	return false;
     }
 
 }
