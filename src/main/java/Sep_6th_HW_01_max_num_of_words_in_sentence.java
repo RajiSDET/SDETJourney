ea3cb7ace3ce504b970d4990924c510350a1730f
rramiah-ext <rramiah-ext@drivewealth.tech>
2023-09-06 18:59:06 +0530

    SEP6ActivityHW1

---
+++ src/main/java/Sep_6th_HW_01_max_num_of_words_in_sentence.java
@@ -0,0 +1,94 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Sep_6th_HW_01_max_num_of_words_in_sentence {
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals(3, mostWordsFound(new String[] {"please wait", "continue to fight", "continue to win"}));
+	}
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals(6, mostWordsFound(new String[] {"alice and bob love leetcode", "i think so too", "this is great thanks very much"}));
+	}
+	
+    public int mostWordsFound(String[] sentences) {
+        
+    	int maxLength=0;
+    	String[] strArray = null;
+    	for (int i = 0; i < sentences.length; i++) {
+    		  
+    		strArray = sentences[i].split(" ");  
+    		if(maxLength<strArray.length)
+    		{
+    			maxLength=strArray.length;
+    		}
+    	}
+    	return maxLength;
+    }
+	
+
+}
+
+/*Pseudo Code
+INPUT - String [] - Array of Strings - Each element is a statement 
+OUTPUT - int Word count of MaxLengthSentence
+
+Logic - 
+
+int length 
+int max_length
+
+Scan through each element of String StringArr[]
+
+Convert each StringArr[i] to another Array of words and fetch the length of this array  
+Store it inside length 
+if(length>max_length)
+{
+max_length=length;
+}
+return max_length
+
+
+
+ * */
+
+/*https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+ 2114. Maximum Number of Words Found in Sentences
+
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+
+ 
+
+Example 1:
+
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation: 
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+Example 2:
+
+Input: sentences = ["please wait", "continue to fight", "continue to win"]
+Output: 3
+Explanation: It is possible that multiple sentences contain the same number of words. 
+In this example, the second and third sentences (underlined) have the same number of words.
+ 
+
+Constraints:
+
+1 <= sentences.length <= 100
+1 <= sentences[i].length <= 100
+sentences[i] consists only of lowercase English letters and ' ' only.
+sentences[i] does not have leading or trailing spaces.
+All the words in sentences[i] are separated by a single space.
+
+ */
--- src/main/java/mandatoryHomeWork/postfoundation/Sep_6th_CW_01_max_mix_array.java
+++ src/main/java/mandatoryHomeWork/postfoundation/Sep_6th_CW_01_max_mix_array.java
@@ -26,6 +26,25 @@ public class Sep_6th_CW_01_max_mix_array {
 		Assert.assertEquals(0,minimizeIt(new int[] {8},2) );
 	}
 	
+	@Test
+	public void test4()
+	{
+		Assert.assertEquals(14,minimizeIt(new int[] {10,4,20,19,9,4,20,14,15,10,9,15,8},9));
+	}
+	
+	@Test
+	public void test5()
+	{
+		Assert.assertEquals(10,minimizeIt(new int[] {11,9,12,16,1,14, 17, 14, 12, 11, 10, 3, 3, 9, 15, 4 ,8},7));
+	}
+	
+	@Test
+	public void test6()
+	{
+		Assert.assertEquals(10,minimizeIt(new int[] {11,9,12,16,1,14, 17, 14, 12, 11, 10, 3, 3, 9, 15, 4 ,8},7));
+	}
+	
+	
 	
 	 public static int minimizeIt(int[] A, int K) {
 	        
@@ -41,11 +60,36 @@ public class Sep_6th_CW_01_max_mix_array {
 				
 			}
 	        
+	       
+	        
 	     Arrays.sort(A);
+	     
+	     for (int i = 0; i < A.length; i++)
+	        {
+	        	System.out.println(A[i]);
+	        }
 		 return A[A.length-1]-A[0];
 	    }
 
 }
+
+//17,13,12,8,7,5,1
+//16,12,
+
+/*
+ 5
+13 9
+10 4 20 19 9 4 20 14 15 10 9 15 8 
+17 7
+11 9 12 16 1 14 17 14 12 11 10 3 3 9 15 4 8 
+18 5
+3 19 4 16 17 5 7 16 3 20 1 20 10 10 2 19 13 13 
+17 7
+2 1 19 19 11 3 7 7 14 14 4 20 1 4 9 20 19 
+10 7
+5 17 15 4 11 15 19 18 7 14
+ *
+ */
 
 /*PSEUDO CODE
 Input 
