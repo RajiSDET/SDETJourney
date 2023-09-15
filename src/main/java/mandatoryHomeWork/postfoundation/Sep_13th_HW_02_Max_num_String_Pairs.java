ebd55dfa5782c85d36c9334b71f38a4bfdef6fe7
rramiah-ext <rramiah-ext@drivewealth.tech>
2023-09-15 16:18:43 +0530

    SEP13ActivityHW2

---
+++ src/main/java/mandatoryHomeWork/postfoundation/Sep_13th_HW_02_Max_num_String_Pairs.java
@@ -0,0 +1,95 @@
+package mandatoryHomeWork.postfoundation;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Sep_13th_HW_02_Max_num_String_Pairs {
+	
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals(2,maximumNumberOfStringPairs(new String[] {"cd","ac","dc","ca","zz"}) );
+	}
+	
+public int maximumNumberOfStringPairs(String[] words) {
+	int counter =0;
+	for (int i = 0; i < words.length-1; i++) {
+		StringBuilder sb = new StringBuilder();
+		for(int j=i+1;j < words.length; j++)
+		{
+		sb.append(words[i]).append(words[j]);
+		System.out.println(sb.toString());
+		System.out.println(sb.reverse().toString());
+		if(sb.toString().equals(sb.reverse().toString()))
+		{
+			counter++;
+		}
+		sb.setLength(0);
+		}
+		
+	}
+      
+	
+	return counter;
+    }
+
+}
+
+/*Pseudo Code
+ Input - String[] words
+ Output - int counter
+ 
+ Approach 
+ initialize a counter 
+ Iterate through the String Array 
+ Have a StringBuilder sb such that sb = sb.append index[i] and index[i+1]
+ if sb and sb.reverse are equal 
+ increment the counter
+ finally return the counter value
+ 
+ */
+
+/*https://leetcode.com/problems/find-maximum-number-of-string-pairs/description/
+
+2744. Find Maximum Number of String Pairs
+You are given a 0-indexed array words consisting of distinct strings.
+
+The string words[i] can be paired with the string words[j] if:
+
+The string words[i] is equal to the reversed string of words[j].
+0 <= i < j < words.length.
+Return the maximum number of pairs that can be formed from the array words.
+
+Note that each string can belong in at most one pair.
+
+ 
+
+Example 1:
+
+Input: words = ["cd","ac","dc","ca","zz"]
+Output: 2
+Explanation: In this example, we can form 2 pair of strings in the following way:
+- We pair the 0th string with the 2nd string, as the reversed string of word[0] is "dc" and is equal to words[2].
+- We pair the 1st string with the 3rd string, as the reversed string of word[1] is "ca" and is equal to words[3].
+It can be proven that 2 is the maximum number of pairs that can be formed.
+Example 2:
+
+Input: words = ["ab","ba","cc"]
+Output: 1
+Explanation: In this example, we can form 1 pair of strings in the following way:
+- We pair the 0th string with the 1st string, as the reversed string of words[1] is "ab" and is equal to words[0].
+It can be proven that 1 is the maximum number of pairs that can be formed.
+Example 3:
+
+Input: words = ["aa","ab"]
+Output: 0
+Explanation: In this example, we are unable to form any pair of strings.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+words[i].length == 2
+words consists of distinct strings.
+words[i] contains only lowercase English letters.*/
