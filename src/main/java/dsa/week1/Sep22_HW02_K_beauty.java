dcc2aa930a107369f74f55484c58596f151b2bd2
rramiah-ext <rramiah-ext@drivewealth.tech>
2023-09-22 06:23:46 +0530

    SEP22ActivityHW2

--- src/main/java/dsa/week1/Sep22_HW02_K_beauty.java
+++ src/main/java/dsa/week1/Sep22_HW02_K_beauty.java
@@ -1,5 +1,46 @@
 package dsa.week1;
 
-public class Sep22_HW02_K_beauty {
+import org.junit.Test;
 
+import junit.framework.Assert;
+
+public class Sep22_HW02_K_beauty {
+	
+	@Test
+	public void test1() {
+		Assert.assertEquals(2, divisorSubstrings(240,2));
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertEquals(2, divisorSubstrings(430043,2));
+	}
+	@Test
+	public void test3() {
+		Assert.assertEquals(1, divisorSubstrings(10,1));
+	}
+	
+	public int divisorSubstrings(int num, int k) {
+	    int k_beauty=0;
+			String Str=""+num;
+					
+			for (int i = 0; i <= Str.length()-k; i++) {
+				StringBuilder sb= new StringBuilder();
+
+				for (int j = i; j < i+k; j++) {
+					sb.append(Str.charAt(j));
+				}
+				int divisor = Integer.parseInt(sb.toString());
+				if(divisor!=0)
+				{
+					if(num%divisor==0)
+					{
+						k_beauty++;
+					}
+				}
+			}
+			
+	      System.out.println(k_beauty);
+	        return  k_beauty; 
+	    }
 }
--- src/main/java/mandatoryHomeWork/postfoundation/Rough.java
+++ src/main/java/mandatoryHomeWork/postfoundation/Rough.java
@@ -1,41 +1,38 @@
 package mandatoryHomeWork.postfoundation;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 public class Rough {
 	
 
-	static int a = Integer.MAX_VALUE;
-	static int b = Integer.MAX_VALUE;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum = a+b;
-	System.out.println(sum);
-		String s = "poiinter";
-		
-	    StringBuilder sb = new StringBuilder();
-		
-	    for (int i = 0; i < s.length(); i++) 
-	    {
-	    	sb.append(s.charAt(i));
-	    	if(s.charAt(i)=='i')
-	    	{
-	    		sb.reverse();
-	    			    		
-	    	}
-			
+		int num = 30003, k = 3;
+		int k_beauty=0;
+		String Str=""+num;
+				
+		for (int i = 0; i <= Str.length()-k; i++) {
+			StringBuilder sb= new StringBuilder();
+
+			for (int j = i; j < i+k; j++) {
+				sb.append(Str.charAt(j));
+			}
+			int divisor = Integer.parseInt(sb.toString());
+			if(divisor!=0)
+			{
+				if(num%divisor==0)
+				{
+					k_beauty++;
+				}
+			}
 		}
-	    
-	    System.out.println(sb.toString().replace("i", ""));
+		
+      System.out.println(k_beauty);
+		
 		
 			
-}}
+}
+	}
 			
 		
 		
